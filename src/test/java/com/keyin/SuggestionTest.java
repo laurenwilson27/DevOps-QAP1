0ad6199ba5d5f25f9720d77c32628607833fbee4
laurenwilson27 <140430039+laurenwilson27@users.noreply.github.com>
2024-05-28 11:53:38 -0230

    Added multiple tests

--- src/test/java/com/keyin/SuggestionTest.java
+++ src/test/java/com/keyin/SuggestionTest.java
@@ -1,8 +1,9 @@
 package com.keyin;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 
 import java.nio.file.Paths;
 
@@ -10,9 +11,37 @@ public class SuggestionTest {
     private final SuggestionEngine testEngine = new SuggestionEngine();
 
     @Test
-    public void testGenerateSuggestions() throws Exception {
-       testEngine.loadDictionaryData(Paths.get(ClassLoader.getSystemResource("words.txt").getPath()));
+    // Simple test for when the letters are right, but in the wrong order
+    public void testWrongLetterOrder() throws Exception {
+        testEngine.loadDictionaryData(Paths.get(ClassLoader.getSystemResource("words.txt").toURI()));
 
-        Assertions.assertTrue(testEngine.generateSuggestions("hellw").contains("hello"));
+        Assertions.assertTrue(testEngine.generateSuggestions("mnoday").contains("monday"));
+    }
+
+    @Test
+    // Test if the engine handles a mistake with multiple obvious suggestions
+    public void testMultipleSuggestions() throws Exception {
+        testEngine.loadDictionaryData(Paths.get(ClassLoader.getSystemResource("words.txt").toURI()));
+
+        String suggestions = testEngine.generateSuggestions("hellp");
+        Assertions.assertTrue(suggestions.contains("hello"));
+        Assertions.assertTrue(suggestions.contains("help"));
+    }
+
+    @Test
+    // The suggestion engine should return no more than 10 words - test this
+    public void testLimit10() throws Exception {
+        testEngine.loadDictionaryData(Paths.get(ClassLoader.getSystemResource("words.txt").toURI()));
+
+        Assertions.assertTrue(testEngine.generateSuggestions("frapple").split("\n").length <= 10);
+    }
+
+    @Test
+    // This one's just silly - used to get this word wrong a lot :)
+    public void testSmorgasbord() throws Exception {
+        testEngine.loadDictionaryData(Paths.get(ClassLoader.getSystemResource("words.txt").toURI()));
+
+        // Expected result is only two words: smorgasbord and smorgasbords
+        Assertions.assertEquals(2, testEngine.generateSuggestions("smorgasborg").split("\n").length);
     }
 }
